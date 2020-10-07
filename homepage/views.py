c0add473a65ee011565b03a78a73d3f49ba05562
Daniel <60117718+DanielMikh@users.noreply.github.com>
2020-10-07 17:26:20 +0300

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -11,7 +11,6 @@ def index(request):
     city_weather = ''
     friend_output = ''
     selected_icecream = ''
-    # В переменную conclusion будет сохранен текст рекомендации
     conclusion = ''
 
     for friend in friends_db:
@@ -33,8 +32,6 @@ def index(request):
         weather = what_weather(city)
         parsed_temperature = what_temperature(weather)
 
-        # Запишите в conclusion
-        # результат вызова функции what_conclusion() с аргументом parsed_temperature
         conclusion = what_conclusion(parsed_temperature)
         friend_output = f'{selected_friend}, тебе прислали {selected_icecream}!'
         city_weather = f'В городе {city} погода: {weather}'
@@ -44,9 +41,7 @@ def index(request):
         'friends': friends,
         'friend_output': friend_output,
         'city_weather': city_weather,
-        'conclusion': conclusion
-        # Передайте значение conclusion в шаблон
-
+        'conclusion': conclusion,
     }
     return render(request, 'homepage/index.html', context)
 
